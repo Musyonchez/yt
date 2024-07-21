027f30c903f89b9c4968870b9bb8b1af7e95d6fb
Musyonchez <musyonchez@gmail.com>
2024-07-21 20:01:25 +0300

    801pm update

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import SearchResults from "./SearchResults";
 import sanitizeSearch from "../utils/sanitizeSearch";
 import WaveLoader from "@/utils/WaveLoader";
+import handleSearch from "../utils/handleSearch";
 
 interface Video {
   originalUrl: string;
@@ -22,9 +23,16 @@ interface PlaylistResponse {
   videos: VideoInfo[];
 }
 
+interface SingleResponse {
+  type: string;
+  videos: VideoInfo[];
+}
+
 const Hero = () => {
   const [searchInput, setSearchInput] = useState("");
-  const [singleResponse, setSingleResponse] = useState(null);
+  const [singleResponse, setSingleResponse] = useState<PlaylistResponse | null>(
+    null
+  );
   const [playlistResponses, setPlaylistResponses] = useState<
     PlaylistResponse[]
   >([]);
@@ -32,78 +40,6 @@ const Hero = () => {
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(event.target.value);
-  };
-
-  const handleSearch = async () => {
-    console.log("Searching for:", searchInput);
-    try {
-      setLoader(true);
-      const sanitizedText = sanitizeSearch({ searchInput });
-      console.log("sanitizedText:", sanitizedText);
-      if (sanitizedText === "single video youtube link") {
-        const response = await axios.post(
-          "http://127.0.0.1:8000/api/v1/url/info/single",
-          {
-            url: searchInput,
-          }
-        );
-        console.log(response.data);
-        setSingleResponse(response.data);
-      } else if (sanitizedText === "playlist youtube link") {
-        const playlistResponse = await axios.post(
-          "http://127.0.0.1:8000/api/v1/url/info/playlist",
-          {
-            url: searchInput,
-          }
-        );
-        console.log(playlistResponse.data);
-
-        // Extracting video URLs from the playlist response
-        const videoUrls = playlistResponse.data.videos.map(
-          (video: Video) => video.originalUrl
-        );
-
-        // Initialize an empty array to hold the fetched video details
-        let fetchedVideoDetails = [];
-
-        // Loop through each URL and fetch details one by one
-        for (const url of videoUrls) {
-          const videoDetailsPromise = axios.post(
-            "http://127.0.0.1:8000/api/v1/url/info/single",
-            {
-              url: url,
-            }
-          );
-
-          console.log("url:", url);
-
-          // Wait for the promise to resolve
-          const videoDetailsResponse = await videoDetailsPromise;
-
-          // Extract the data from the response
-          const videoDetailsData = videoDetailsResponse.data;
-          console.log("videoDetailsData:", videoDetailsData);
-
-          // Add the fetched video details to the array
-          fetchedVideoDetails.push(videoDetailsData);
-
-          // Update the state with the new details
-          setPlaylistResponses(fetchedVideoDetails);
-          console.log("Updated setPlaylistResponses:", fetchedVideoDetails);
-        }
-        console.log("PlaylistResponses:", playlistResponses);
-
-        // After the loop ends, setPlaylistResponses should contain details for all videos
-      } else if (sanitizedText === "textsearch") {
-        console.log("word search", sanitizedText);
-      } else {
-        console.log("cant search");
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setLoader(false);
-    }
   };
 
   return (
@@ -117,7 +53,17 @@ const Hero = () => {
           onChange={handleInputChange}
           required
         />
-        <button className="px-3" onClick={handleSearch}>
+        <button
+          className="px-3"
+          onClick={() =>
+            handleSearch(
+              searchInput,
+              setLoader,
+              setSingleResponse,
+              setPlaylistResponses
+            )
+          }
+        >
           Search
         </button>
       </div>
---
+++ client/src/utils/handleSearch.tsx
@@ -0,0 +1,94 @@
+import axios from "axios";
+import sanitizeSearch from "./sanitizeSearch";
+
+interface Video {
+  originalUrl: string;
+}
+
+interface VideoInfo {
+  originalUrl: string;
+  title: string;
+  channel: string;
+  duration: number; // Duration in seconds
+  thumbnail: string;
+}
+
+interface PlaylistResponse {
+  type: string;
+  videos: VideoInfo[];
+}
+
+interface SingleResponse {
+  type: string;
+  videos: VideoInfo[];
+}
+
+const handleSearch = async (
+  searchInput: string,
+  setLoader: (value: boolean) => void,
+  setSingleResponse: (data: SingleResponse | null) => void,
+  setPlaylistResponses: (data: PlaylistResponse[]) => void
+) => {
+  console.log("Searching for:", searchInput);
+  try {
+    setLoader(true);
+    const sanitizedText = sanitizeSearch({ searchInput });
+    console.log("sanitizedText:", sanitizedText);
+    if (sanitizedText === "single video youtube link") {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/v1/url/info/single",
+        {
+          url: searchInput,
+        }
+      );
+      console.log(response.data);
+      setSingleResponse(response.data);
+    } else if (sanitizedText === "playlist youtube link") {
+      const playlistResponse = await axios.post(
+        "http://127.0.0.1:8000/api/v1/url/info/playlist",
+        {
+          url: searchInput,
+        }
+      );
+      console.log(playlistResponse.data);
+
+      const videoUrls = playlistResponse.data.videos.map(
+        (video: Video) => video.originalUrl
+      );
+
+      let fetchedVideoDetails = [];
+
+      for (const url of videoUrls) {
+        const videoDetailsPromise = axios.post(
+          "http://127.0.0.1:8000/api/v1/url/info/single",
+          {
+            url: url,
+          }
+        );
+
+        console.log("url:", url);
+
+        const videoDetailsResponse = await videoDetailsPromise;
+
+        const videoDetailsData = videoDetailsResponse.data;
+        console.log("videoDetailsData:", videoDetailsData);
+
+        fetchedVideoDetails.push(videoDetailsData);
+
+        console.log("Updated setPlaylistResponses:", fetchedVideoDetails);
+        setPlaylistResponses(fetchedVideoDetails);
+      }
+      console.log("PlaylistResponses:", fetchedVideoDetails);
+    } else if (sanitizedText === "textsearch") {
+      console.log("word search", sanitizedText);
+    } else {
+      console.log("can't search");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  } finally {
+    setLoader(false);
+  }
+};
+
+export default handleSearch;
